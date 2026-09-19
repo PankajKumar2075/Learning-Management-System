1b5ff5c088b697079f981ee51b59d0cbadee9918
PankajKumar2075 <pankajnehra646@gmail.com>
2026-09-19 21:08:44 +0530

    Delete front-end directory

--- front-end/src/hooks/useNetworkStatus.js
+++
@@ -1,7 +0,0 @@
-// src/hooks/useNetworkStatus.js
-import { useSyncExternalStore } from "react";
-import { getSnapshot, subscribe } from "../utils/networkStore";
-
-export default function useNetworkStatus() {
-  return useSyncExternalStore(subscribe, getSnapshot);
-}
